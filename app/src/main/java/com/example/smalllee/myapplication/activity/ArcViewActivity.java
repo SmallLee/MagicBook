3a10f79990766ab6e6cfd8eb9b8c0519b7c0e521
lixiaoniu <lixiaoniu@edaijia-inc.cn>
2019-01-08 09:58:38 +0800

    弧线

---
+++ app/src/main/java/com/example/smalllee/myapplication/activity/ArcViewActivity.java
@@ -0,0 +1,15 @@
+package com.example.smalllee.myapplication.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.smalllee.myapplication.R;
+
+public class ArcViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arc_view);
+    }
+}
